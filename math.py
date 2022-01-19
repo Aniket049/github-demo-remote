33f7ab1928e0b76b91f1ac166fe7d2ef8ceb23a9
Aniket049 <55133424+Aniket049@users.noreply.github.com>
2022-01-19 16:21:21 +0530

    Initial commit of math.py

---
+++ math.py
@@ -0,0 +1,8 @@
+def add(x,y): #implementation of addition
+    pass
+def subtract(x,y):  #implementation of substraction
+    pass
+def multiply(x,y):  #implementation of multiplication
+    pass
+def divide(x,y):
+    pass
