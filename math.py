c42525dc5e54be97a19be5274f24c998ae23a560
Aniket049 <55133424+Aniket049@users.noreply.github.com>
2022-01-19 17:15:54 +0530

    Add header in substract() fn in main branch

--- math.py
+++ math.py
@@ -2,7 +2,7 @@ def add(x,y): #implementation of addition
     z=x+y
     return z
 def subtract(x,y):  #implementation of substraction
-    z=x-y
+    z=x-y #store the difference between the numbers
     return z
 def multiply(x,y):  #implementation of multiplication
     z=x*y
